ec9caf05ac7fabb18f772c46012d777a8e6ca874
tushartambatkar <tushartambatkar777@gmail.com>
2024-04-29 15:33:17 +0530

    code

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -46,6 +46,7 @@ public class SpiralMatrix {
                        {5, 6, 7, 8,},
                        {9, 10, 11, 12,},
                        {13, 14, 15, 16}};
-         printSpiral(matrix);
+                       
+    printSpiral(matrix);
   }
 }
